dc3c9d542d2fe3541348a7dca28a23b883b2502a
Michael Phelps <michael.phelps@cpcc.edu>
2020-12-02 00:17:03 -0500

    Day 2 part 2 solution

--- day2.py
+++ day2.py
@@ -2,18 +2,26 @@ with open('day2_input.txt', 'r') as f:
     POLICY_TO_PASSWORDS = tuple(x.strip().split(": ") for x in f.readlines())
 
 
-def count_valid(policy_to_passwords):
+def count_valid(policy_to_passwords, validator):
     valid = 0
     for policy, password in policy_to_passwords:
-        valid += is_valid(password, policy)
+        valid += validator(password, policy)
     return valid
 
 
-def is_valid(password, policy):
+def validator_1(password, policy):
     _range, char = policy.split()
     a, b = _range.split('-')
     return password.count(char) in range(int(a), int(b)+1)
 
+def validator_2(password, policy):
+    pos, char = policy.split()
+    a, b = pos.split('-')
+    try:
+        return (password[int(a)-1] == char) ^ (password[int(b)-1] == char)
+    except IndexError:
+        return False
 
 if __name__ == '__main__':
-    print(count_valid(POLICY_TO_PASSWORDS))
+    print(count_valid(POLICY_TO_PASSWORDS, validator=validator_1))
+    print(count_valid(POLICY_TO_PASSWORDS, validator=validator_2))
